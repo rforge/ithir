1c9489d8c2f5706ee7c0bc66e8fe7bb3a3f14dfe
brendo1001 <brendo1001@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-13 11:42:19 +0000

    add plot soil profile
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ithir@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/plot_soilProfile.R
@@ -0,0 +1,19 @@
+# Purpose        : Plot raw soil profile data
+# Maintainer     : Brendan Malone (brendan.malone@sydney.edu.au); 
+# Contributions  : 
+# Status         : working
+# Note           : 
+
+
+##### Function for plotting raw soil profile data values
+plot_soilProfile <- function(vals, depths, data, label= "") {
+  matX<- matrix(NA, nrow=nrow(data), ncol= 4)
+  matY<- matrix(NA, nrow=nrow(data), ncol= 4)
+  for (i in 1: nrow(data)){
+    matX[i,]<- c(vals[i]-vals[i], vals[i], vals[i], vals[i]-vals[i]) 
+    matY[i,]<- c(depths[i,1], depths[i,1], depths[i,2], depths[i,2]) }
+  raw.plot<-plot(matX[1,], matY[1,],type="n",ylim=c(max(depths),0),xlim=c(min(vals), (max(vals)*1.1)),main=paste("soil profile:",data[1,1], sep=" "), ylab="depth", xlab= label, lty=2, lwd=3, xaxs="i", col="black", font.lab=2,cex.lab=1.5)
+  for (i in 1: nrow(data)){
+    polygon (matX[i,],matY[i,], lty=1, lwd=3, border="black") }}
+
+#END OF SCRIPT
